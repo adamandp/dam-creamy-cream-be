1fe2f4420c8e0dd54091167db82a908c7c481ac8
Adam Ananda Putra <adamanandaputra@gmail.com>
2026-09-14 17:09:57 +0700

    feat: add dedicated prisma service

---
+++ src/common/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(configService: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: configService.getOrThrow('DATABASE_URL'),
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
